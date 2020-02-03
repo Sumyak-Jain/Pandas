bb791f200d382049694d746f71aaa924615f1d83
SUMYAK JAIN <3456gaurav@gmail.com>
2020-02-03 10:47:42 +0530

    problem 1

---
+++ problem1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class hash1 {
+    public static void main(String[] args) {
+        
+    
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int arr[]=new int[b];
+    for( int i=0;i<b;i++){
+        arr[i]=sc.nextInt();
+    
+}   
+    
+    } 
+}
